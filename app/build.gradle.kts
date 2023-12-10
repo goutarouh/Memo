e9727ae41fbd27c0f50148d4cbeada3e736d7166
goutarouh <gouta.gorogoro@gmail.com>
2023-12-10 12:47:53 +0900

    #1 Add Compose.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,14 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+
+    buildFeatures {
+        compose = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.4.2"
+    }
 }
 
 dependencies {
@@ -49,6 +57,13 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
+    val composeBom = platform("androidx.compose:compose-bom:2023.01.00")
+    implementation(composeBom)
+    implementation("androidx.compose.foundation:foundation")
+    implementation("androidx.compose.material:material")
+    implementation("androidx.activity:activity-compose:1.8.1")
+
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
--- app/src/main/java/com/github/goutarouh/memo/MainActivity.kt
+++ app/src/main/java/com/github/goutarouh/memo/MainActivity.kt
@@ -2,10 +2,29 @@ package com.github.goutarouh.memo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+//        setContent {
+//            MainScreen()
+//        }
+    }
+}
+
+@Composable
+fun MainScreen() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(text = "Hello World from Compose")
     }
 }
