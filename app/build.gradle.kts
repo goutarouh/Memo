033f20725ee0a5ba71c32a28dea141f8be701b67
goutarouh <gouta.gorogoro@gmail.com>
2023-12-10 12:23:31 +0900

    #1 Add Roborazzi screenshot function in test.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("io.github.takahirom.roborazzi")
 }
 
 android {
@@ -26,6 +27,13 @@ android {
             )
         }
     }
+
+    testOptions {
+        unitTests {
+            isIncludeAndroidResources = true
+        }
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
@@ -41,7 +49,13 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    testImplementation("junit:junit:4.13.2")
+    testImplementation("org.robolectric:robolectric:4.11.1")
+    testImplementation("androidx.test.espresso:espresso-core:3.1.0")
+
+    testImplementation("io.github.takahirom.roborazzi:roborazzi:1.7.0-rc-1")
+    testImplementation("io.github.takahirom.roborazzi:roborazzi-junit-rule:1.7.0-rc-1")
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,11 @@
 plugins {
     id("com.android.application") version "8.1.0" apply false
     id("org.jetbrains.kotlin.android") version "1.8.0" apply false
+    id("io.github.takahirom.roborazzi") version "1.7.0-rc-1" apply false
+}
+
+buildscript {
+    dependencies {
+        classpath("io.github.takahirom.roborazzi:roborazzi-gradle-plugin:1.7.0-rc-1")
+    }
 }
