80cbba6295ffc6ebb87b3468e88939781faa81d0
goutarouh <gouta.gorogoro@gmail.com>
2023-12-16 13:22:32 +0900

    #8 Add error handling.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,6 +61,7 @@ dependencies {
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
+    implementation("androidx.compose.material3:material3:1.1.2")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 
     implementation("androidx.fragment:fragment-ktx:1.6.2")
