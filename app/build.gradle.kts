c90f4dc7003a91c9fd6748c2399c6b49a374660e
goutarouh <gouta.gorogoro@gmail.com>
2023-12-17 17:24:43 +0900

    #8 Add test module.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,6 +57,7 @@ android {
 dependencies {
 
     implementation(project(":data:repository"))
+    testImplementation(project(":test"))
 
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
